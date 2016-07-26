3564145a23d0c4bdd1e41f94e1a1f5691a5a8303
Arthur Cougé <arthur.couge@gmail.com>
2016-07-26 09:54:41 +0200

    1.3.3
    patch of the .bat file

--- Build ConnecShop.bat
+++ Build ConnecShop.bat
@@ -1,6 +1,7 @@
 cd C:\Users\ac01.octave\Desktop\ConnecShop\1.0.0\
 
 xcopy build C:\Users\ac01.octave\Desktop\ConnecShop_cordova\www\build /R /Y
+xcopy app C:\Users\ac01.octave\Desktop\ConnecShop_cordova\www\app/R /Y /E
 xcopy styles\css C:\Users\ac01.octave\Desktop\ConnecShop_cordova\www\styles\css /R /Y
 xcopy index.html C:\Users\ac01.octave\Desktop\ConnecShop_cordova\www /F /Y
 
